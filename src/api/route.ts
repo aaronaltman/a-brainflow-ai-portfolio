fabcdfc34de113af3a9ab405edcb5bc8f79176fe
aka <aaronkaltman@gmail.com>
2023-12-26 20:21:07 -0500

    create note schema

--- src/api/route.ts
+++ src/api/route.ts
@@ -5,6 +5,9 @@ export async function Post(req: Request) {
     const body = req.body;
     // const note = noteSchema.parse(body);
     const parseResult = createNoteSchema.safeParse(body);
+    if (!parseResult.success) {
+      return { status: 400, body: { error: parseResult.error } };
+    }
   } catch (error) {
     console.log(error, "error");
     return { status: 500, body: { error: error } };
--- src/lib/validation/note.ts
+++ src/lib/validation/note.ts
@@ -2,13 +2,7 @@ import z from "zod";
 
 export const createNoteSchema = z.object({
   title: z.string().min(1, { message: "title required" }).max(100),
-  content: z.string().optional().max(1000),
-  tags: z.array().items(z.string()),
-  color: Joi.string().required(),
-  createdAt: Joi.string().required(),
-  updatedAt: Joi.string().required(),
-  id: Joi.string().required(),
-  userId: Joi.string().required(),
+  content: z.string().optional(),
 });
 
-export type Note = z.infer<typeof noteSchema>;
+export type Note = z.infer<typeof createNoteSchema>;
