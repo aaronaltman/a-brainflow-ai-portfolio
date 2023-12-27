e001f34d01ffbcfa3b8a937d82ba0638b073cfb8
aka <aaronkaltman@gmail.com>
2023-12-26 19:28:52 -0500

    home page css

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Page() {
-  return <div>P</div>;
+  return (
+    <main>
+      <section>
+        <h1>Notes</h1>
+      </section>
+      P
+    </main>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import React from "react";
 
 export default function Home() {
   return (
-    <section>
-      <div>hello world </div>
-    </section>
+    <main className="p-20 text-xl text-bold flex justify-center">
+      <section className="">
+        <h1 className="">Chatbot Demo - Aaron K. Altman</h1>
+      </section>
+    </main>
   );
 }
